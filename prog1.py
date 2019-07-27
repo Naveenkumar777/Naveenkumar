eb08cdf7f1da8d523d9b708ace95e474611fc904
Naveenkumar777 <52481464+Naveenkumar777@users.noreply.github.com>
2019-07-27 21:29:19 +0530

    Create prog1.py

---
+++ prog1.py
@@ -0,0 +1,12 @@
+c=input()
+a=[]
+x=[]
+for i in range(0,len(c)):
+    a.append(c[i])
+for i in range(0,len(c)):
+    if(a.count(a[i])>1):
+        x.append(a[i])
+g=list(set(x))
+print(len(g))
+                
+                    
