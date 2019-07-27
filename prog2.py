71d17c76bdfcfd8ebe5578ff4a690ba718bbab20
Naveenkumar777 <52481464+Naveenkumar777@users.noreply.github.com>
2019-07-27 21:23:46 +0530

    Create prog2.py

---
+++ prog2.py
@@ -0,0 +1,13 @@
+N=int(input())
+a=[]
+x=[]
+for i in range(0,N):
+    b=input()
+    a.append(b)
+for i in range(0,N):
+    if(a.count(a[i])>1):
+        x.append(a[i])
+g=list(set(x))
+print(len(g))
+                
+                    
