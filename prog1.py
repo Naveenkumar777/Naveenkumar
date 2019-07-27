d0e684d4045356b2d752772a47911542b766cf64
Naveenkumar777 <52481464+Naveenkumar777@users.noreply.github.com>
2019-07-27 21:57:09 +0530

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -3,13 +3,21 @@ Y=input()
 a=[]
 b=[]
 s=[]
+g=[]
 for i in range(0,len(X)):
     a.append(X[i])
 for i in range(0,len(Y)):
     b.append(Y[i])
-for i in range(0,len(X)):
-    if(X[i]==Y[i]):
-        s.append(Y[i])
-f=len(b)-len(s)
-print(f)
+if(len(a)<len(b)):
+    
+    for i in range(0,len(X)):
+        if(X[i]==Y[i]):
+            s.append(Y[i])
+    f=len(b)-len(s)
+    print(f)
+else:
+    for i in range(0,len(Y)):
+        if(Y[i]==X[i]):
+            g.append(Y[i])
+    print(len(g))
                     
