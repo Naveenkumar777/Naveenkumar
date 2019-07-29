e556ca127e8e60b8af3440ca28050e15cd9f2c3c
Naveenkumar777 <52481464+Naveenkumar777@users.noreply.github.com>
2019-07-29 10:34:59 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,19 @@
+n,q=input().split()
+n=int(n)
+q=int(q)
+sum1=0
+temp=0
+a=list(map(int,input().split()))
+for i in range(0,len(a)-1):
+    for j in range(i+1,len(a)):
+        sum1=a[i]+a[j]
+        if(sum1==q):
+            temp=1
+            print("yes")
+            break
+    if(temp==1):
+        break
+    
+        
+        
+    
