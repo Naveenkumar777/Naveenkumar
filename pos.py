9f6c097581023f809a1fab60f2307397d3ad563f
Naveenkumar777 <52481464+Naveenkumar777@users.noreply.github.com>
2019-08-05 15:56:26 +0530

    Create pos.py

---
+++ pos.py
@@ -0,0 +1,14 @@
+n=int(input())
+q=input().split()
+a=[]
+b=[]
+for i in range(0,len(q)):
+    a.append(int(q[i]))
+for i in range(0,len(a)):
+    if(a[i]%2==0):
+        if(i%2==1):
+            b.append(a[i])
+    else:
+        b.append(a[i])
+for i in range(0,len(b)):
+    print(b[i],end=" ")
