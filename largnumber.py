b8e8de03b8548dbd1fda5e41282f71618f24d769
Naveenkumar777 <52481464+Naveenkumar777@users.noreply.github.com>
2019-08-05 15:30:50 +0530

    Update largnumber.py

--- largnumber.py
+++ largnumber.py
@@ -1,9 +1,9 @@
 n=int(input())
 q=input().split()
-x=[]
-y=[]
+a=[]
+b=[]
 for i in range(0,len(q)):
-    x.append(q[i])
-y=sorted(x)
-for i in range(len(y)-1,-1,-1):
-    print(y[i],end="")
+    a.append(q[i])
+b=sorted(a)
+for i in range(len(b)-1,-1,-1):
+    print(b[i],end="")
