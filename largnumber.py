11292163bec4157aa4d5a6bbf0fc98e5129815e4
Naveenkumar777 <52481464+Naveenkumar777@users.noreply.github.com>
2019-08-05 15:34:29 +0530

    Update largnumber.py

--- largnumber.py
+++ largnumber.py
@@ -5,5 +5,8 @@ b=[]
 for i in range(0,len(q)):
     a.append(q[i])
 b=sorted(a)
-for i in range(len(b)-1,-1,-1):
-    print(b[i],end="")
+if(a.count(a[0])==len(a)):
+    print(a[0])
+else:
+    for i in range(len(b)-1,-1,-1):
+        print(b[i],end="")
