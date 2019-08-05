19a390ad92a215e18601afb633cb07c1556308a3
Naveenkumar777 <52481464+Naveenkumar777@users.noreply.github.com>
2019-08-05 20:27:07 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,8 +1,19 @@
 q=input()
 a=[]
+b=[]
 for i in range(0,len(q)):
     a.append(q[i])
-b=list(set(a))
-c=sorted(b)
-for i in range(0,len(c)):
-    print(c[i],end="")
+for i in range(0,len(a)):
+    if(a.count(a[i])>1):
+        b.append(a[i])
+    else:
+        b.append(a[i])
+c=list(set(b))
+if(len(c)!=len(a)):
+    
+    d=sorted(c)
+    for i in range(0,len(d)):
+        print(d[i],end="")
+else:
+    for i in range(0,len(b)):
+        print(b[i],end="")
