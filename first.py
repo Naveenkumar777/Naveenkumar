00a1a18894af86a6e19a5eb9a7784f8d57b0c66e
Naveenkumar777 <52481464+Naveenkumar777@users.noreply.github.com>
2019-08-05 15:44:51 +0530

    Update first.py

--- first.py
+++ first.py
@@ -2,9 +2,16 @@ n=int(input())
 q=input().split()
 a=[]
 b=[]
+c=[]
 for i in range(0,len(q)):
     a.append(q[i])
 for i in range(0,len(a)):
     if(a.count(a[i])>1):
         b.append(a[i])
-print(b[0])
+for i in range(0,len(a)):
+    if(a.count(a[i])==1):
+        c.append(a[i])
+if(len(c)==len(a)):
+    print("unique")
+else:
+    print(b[0])
